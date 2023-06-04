52121c7791d559f60794ec35d573a496d25a9968
Kara Fang <kara.fang1@gmail.com>
2023-06-04 01:55:04 -0400

    update HomeIntro page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import HomeIntro from '../components/HomeIntro';
+import SearchWorkouts from '../components/SearchWorkouts';
+import Workouts from '../components/Workouts';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+        <HomeIntro />
+        <SearchWorkouts />
+        <Workouts />
+    </Box>
   )
 }
 
