bbf84380941217339bebca674b1389ead3bcdf7a
Kara Fang <kara.fang1@gmail.com>
2023-06-06 17:36:43 -0400

    add pagination to the page

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -6,8 +6,19 @@ import { workoutOptions, fetchData } from '../Utils/fetchData';
 import WorkoutCard from './WorkoutCard';
 
 const Workouts = ({ workouts, setWorkout, bodyPart }) => {
-    console.log(workouts);
-    
+    const [currentPage, setCurrentPage] =useState(1);
+    const workoutsPerPage = 9;
+
+    const indexOfLastWorkout = currentPage * workoutsPerPage;
+    const indexOfFirstWorkout = indexOfLastWorkout - workoutsPerPage;
+    const currentWorkouts = workouts.slice(indexOfFirstWorkout, indexOfLastWorkout);
+
+    const paginate = (e, value) => {
+        setCurrentPage(value);
+        window.scrollTo({ top: 1800, behavior: 'smooth' })
+
+    }
+
   return (
     <box id="workouts"
         sx={{mt: {lg: '110px'}}}
@@ -19,9 +30,23 @@ const Workouts = ({ workouts, setWorkout, bodyPart }) => {
         </Typography>
         <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}}
         flexWrap="wrap" justifyContent="center">
-            {workouts.map((workout, index) => (
+            {currentWorkouts.map((workout, index) => (
                 <WorkoutCard key={index} workout={workout} />
             ))}
+
+        </Stack>
+        <Stack mt="100px" alignItems="center">
+                {workouts.length > 9 && (
+                    <Pagination
+                        color="standard"
+                        shape="rounded"
+                        defaultPage={1}
+                        count={Math.ceil(workouts.length / workoutsPerPage)}
+                        page={currentPage}
+                        onChange={(paginate)}
+                        size="large"
+                    />
+                )}
 
         </Stack>
     </box>
